e330bacb49885121c4ffcd44a65fee780c5e598d
blackkillers20 <77771541+blackkillers20@users.noreply.github.com>
2023-02-14 16:20:46 +0700

    ReStructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,28 +3,15 @@ import { RouterModule } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './modules/admin/admin.module';
-import { UserModule } from './modules/users/users.module';
-import { CreateUsersModule } from './modules/users/CreateUsers.module';
+
 
 @Module({
   imports: [
     AdminModule,
-    UserModule,
-    CreateUsersModule,
     RouterModule.register([
       {
         path: 'admin',
         module: AdminModule,
-        children: [
-          {
-          path: 'users',
-          module: UserModule,
-          },
-          {
-            path: 'createUsers',
-            module: CreateUsersModule,
-          }
-        ]
       },
     ])
   ],
--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { Response } from 'express';
 
 @Controller()
 export class AdminController {
   constructor(private readonly appService: AdminService) {}
 
   @Get()
-  @Render('admin_index')
-  root() {
-    return { };
+  root(@Res() res: Response) {
+    return res.render('roles/admin/dashboard', { layout: 'main' });
+  }
+
+  @Get('users')
+  users(@Res() res: Response) {
+    return res.render('roles/admin/user_index', { layout: 'auth' });
+  }
+
+  @Get('createUsers')
+  createUsers(@Res() res: Response) {
+    return res.render('roles/admin/CreateUsers_index', { layout: 'main' });
+  }
+
+  @Get('Organization')
+  Organization(@Res() res: Response) {
+    return res.render('organizations/dashboard', { layout: 'auth' });
   }
 }
