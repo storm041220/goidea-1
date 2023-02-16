71d51f32b73cb2a976432e49ea0262e7c94ee276
bakaemon <69375806+bakaemon@users.noreply.github.com>
2023-02-16 15:42:49 +0700

    added global database module

--- src/common/database_module/database.module.ts
+++ src/common/database_module/database.module.ts
@@ -1,2 +1,36 @@
 import { MongooseModule } from "@nestjs/mongoose";
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
+import { AccountSchema } from "@src/modules/accounts/schema/account.schema";
+import { DefaultAccount } from "../util/default-account";
+
+@Global()
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: "Account", schema: AccountSchema
+            },
+        ]),
+        MongooseModule.forFeatureAsync([
+            {
+                name: "Account",
+                useFactory: () => {
+                    const schema = AccountSchema;
+                    const accountModel = new DefaultAccount(schema);
+                    accountModel.generate().then(_ => { });
+                    return schema;
+                }
+            }
+        ])
+    ],
+
+    exports: [
+        MongooseModule.forFeature([
+            {
+                name: "Account", schema: AccountSchema
+            },
+        ])
+    ]
+})
+export class DatabaseModule {
+}
