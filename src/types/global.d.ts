400ea8aa211e9f2fa517dad5c6572be975248368
bakaemon <69375806+bakaemon@users.noreply.github.com>
2023-02-16 15:42:24 +0700

    added global types

---
+++ src/types/global.d.ts
@@ -0,0 +1,66 @@
+declare module "mongoose" {
+    interface QueryOptions {
+        select?: string;
+        populate?: PopulateOptions | (PopulateOptions | string)[];
+        nullable?: boolean;
+    }
+}
+
+declare module "mongoose" {
+    interface PaginateOptions {
+        select?: string;
+        populate?: PopulateOptions | (PopulateOptions | string)[];
+        nullable?: boolean;
+    }
+}
+
+type ReactionResponse = {
+    code: string;
+    quantity: number;
+    author: any[]
+    [index: string]: any;
+}
+
+type VoteTypes = "none" | "upvote" | "downvote";
+
+interface PaginatedDocumentsResponse<T> {
+    data: T[];
+    paginationOptions: Partial<import("mongoose").PaginateResult<T>>;
+}
+
+interface ResponseBody {
+    message?: string;
+    data?: any;
+    success?: boolean;
+    paginationOptions?: Partial<import("mongoose").PaginateResult<T>>;
+}
+
+interface AuthConfig {
+    grantType: string,
+    redirectUri: string,
+    clientID: string,
+    clientSecret: string,
+}
+
+interface AuthData {
+    grantType?: string,
+    clientID?: string,
+    clientSecret?: string,
+    refreshToken?: string,
+    code?: string,
+}
+
+interface TokenPayload {
+    teamId?: string,
+    accountId: string
+}
+
+interface SSOConfig {
+    grantType: string,
+    redirectUri: string,
+    authorizationCode: string,
+}
+
+interface NotificationOptions {
+    save?: boolean;
+}
