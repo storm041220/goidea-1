768bca7bda4e0f9245ef15f8a90772cefb5daca8
bakaemon <69375806+bakaemon@users.noreply.github.com>
2023-02-19 16:50:15 +0700

    added env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './modules/admin/admin.module';
 import { IdeaModule } from './modules/ideas/idea.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './common/database_module/database.module';
+import { MONGO_URI } from './configs/env';
+import { TokensModule } from './modules/token/token.modulet';
+import { ConfigModule } from '@nestjs/config';
+import configs from './configs';
 
 console.log(`MONGO_URI: ${MONGO_URI}`);
 
@@ -14,6 +20,10 @@ console.log(`MONGO_URI: ${MONGO_URI}`);
     TokensModule,
     AdminModule,
     IdeaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configs]
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
